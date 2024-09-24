9475003e5ce67f065606d8b115926fb35d01b6d5
Dev-N-X <kalil.about@gmail.com>
2024-09-24 08:33:01 +0200

    first commit

---
+++ clicker/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace CookieClickerApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.buttonClickMe = new System.Windows.Forms.Button();
+            this.labelCookies = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonClickMe
+            // 
+            this.buttonClickMe.Location = new System.Drawing.Point(14, 15);
+            this.buttonClickMe.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonClickMe.Name = "buttonClickMe";
+            this.buttonClickMe.Size = new System.Drawing.Size(112, 38);
+            this.buttonClickMe.TabIndex = 0;
+            this.buttonClickMe.Text = "Click me!";
+            this.buttonClickMe.UseVisualStyleBackColor = true;
+            this.buttonClickMe.Click += new System.EventHandler(this.button_Click);
+            // 
+            // labelCookies
+            // 
+            this.labelCookies.AutoSize = true;
+            this.labelCookies.Location = new System.Drawing.Point(14, 69);
+            this.labelCookies.Name = "labelCookies";
+            this.labelCookies.Size = new System.Drawing.Size(83, 20);
+            this.labelCookies.TabIndex = 1;
+            this.labelCookies.Text = "Cookies: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 306);
+            this.Controls.Add(this.labelCookies);
+            this.Controls.Add(this.buttonClickMe);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Cookie Clicker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button buttonClickMe;
+        private System.Windows.Forms.Label labelCookies;
+    }
+}
---
+++ clicker/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CookieClickerApp
+{
+    public partial class Form1 : Form
+    {
+        private int cookieCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent(); // Make sure this line is present to call the designer method
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            cookieCount++;
+            labelCookies.Text = "Cookies: " + cookieCount;
+        }
+    }
+}
